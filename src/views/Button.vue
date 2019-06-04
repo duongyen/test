65918517e5c8669535ef3d70aaf551728f3eaea5
Duong Yen <yenduong969@gmail.com>
2019-06-04 14:15:53 +0700

    first commit

---
+++ src/views/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="button">
+        <h1>This is an Button </h1>
+        <ExButton />
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+import ExButton from '@/components/ExButton.vue'
+export default {
+  name: 'home',
+  components: {
+    ExButton
+  }
+}
+
+</script>
